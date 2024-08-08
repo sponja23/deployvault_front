8eb539a70dee2e3aae8acf8dde43e1f71b088421
pedroaugdans <pedro,aug,dans@gmail.com>
2024-08-08 21:18:38 +0200

    trying to fix loginrequest model issue

--- src/features/Authentication/useAuthForm.tsx
+++ src/features/Authentication/useAuthForm.tsx
@@ -22,7 +22,7 @@ export const useAuthForm = () => {
   const [login] = useLoginMutation();
   const [register] = useSignupMutation();
   const isLoading = useAppSelector(selectIsLoading);
-  const initialState: InitialState = pathname === "auth" ? { email: "", password: "" } : { username: "", email: "", password: "" };
+  const initialState: InitialState = pathname === "auth" ? { username: "", email: "", password: "" } : { username: "", email: "", password: "" };
 
   /**
    * Handles the form submission.
