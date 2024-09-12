280cea5566aca9a8626ab3441e70a2226047751a
sponja23 <tomas.spognardi@gmail.com>
2024-09-11 21:17:04 -0300

    Ran prettier
    Co-authored-by: nachovigilante <nachovigilante@gmail.com>

--- src/features/Authentication/RequireAuth.tsx
+++ src/features/Authentication/RequireAuth.tsx
@@ -2,16 +2,16 @@ import { Outlet, Navigate, useLocation } from "react-router-dom";
 import useAuth from "../../auth/useAuth";
 
 export const RequireAuth = () => {
-    const location = useLocation();
-    const { isAuthenticated } = useAuth();
+  const location = useLocation();
+  const { isAuthenticated } = useAuth();
 
-    // TODO: Loading for authentication? Maybe not necessary
-    // if (loading) {
-    //     return <CaosSpinner />;
-    // }
-    return isAuthenticated ? (
-        <Outlet />
-    ) : (
-        <Navigate to="/auth" state={{ from: location }} replace />
-    );
+  // TODO: Loading for authentication? Maybe not necessary
+  // if (loading) {
+  //     return <CaosSpinner />;
+  // }
+  return isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/auth" state={{ from: location }} replace />
+  );
 };
--- src/features/Authentication/useAuthForm.tsx
+++ src/features/Authentication/useAuthForm.tsx
@@ -4,9 +4,9 @@ import { loginSchema, registerSchema } from "../../util/validationSchema";
 import useAuth from "../../auth/useAuth";
 
 interface FormState {
-    email: string;
-    password: string;
-    username: string;
+  email: string;
+  password: string;
+  username: string;
 }
 
 /**
@@ -15,37 +15,37 @@ interface FormState {
  * @returns An object containing the Formik instance and the current pathname.
  */
 export const useAuthForm = () => {
-    const navigate = useNavigate();
-    const { pathname } = useLocation();
-    const { login, register } = useAuth();
-    const initialState: FormState =
-        pathname === "auth"
-            ? { username: "", email: "", password: "" }
-            : { username: "", email: "", password: "" };
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { login, register } = useAuth();
+  const initialState: FormState =
+    pathname === "auth"
+      ? { username: "", email: "", password: "" }
+      : { username: "", email: "", password: "" };
 
-    /**
-     * Handles the form submission.
-     *
-     * @param values - The form values.
-     * @param setSubmitting - A function to set the submitting state.
-     */
-    const handleSubmit = async (
-        values: FormState,
-        { setSubmitting }: FormikHelpers<FormState>
-    ) => {
-        if (pathname === "/auth") {
-            await login(values.email, values.password);
-        } else {
-            await register(values.username, values.email, values.password);
-        }
-        navigate("/home");
-        setSubmitting(false);
-    };
+  /**
+   * Handles the form submission.
+   *
+   * @param values - The form values.
+   * @param setSubmitting - A function to set the submitting state.
+   */
+  const handleSubmit = async (
+    values: FormState,
+    { setSubmitting }: FormikHelpers<FormState>,
+  ) => {
+    if (pathname === "/auth") {
+      await login(values.email, values.password);
+    } else {
+      await register(values.username, values.email, values.password);
+    }
+    navigate("/home");
+    setSubmitting(false);
+  };
 
-    const formik = useFormik({
-        initialValues: initialState,
-        validationSchema: pathname === "/auth" ? loginSchema : registerSchema,
-        onSubmit: handleSubmit,
-    });
-    return { formik, pathname };
+  const formik = useFormik({
+    initialValues: initialState,
+    validationSchema: pathname === "/auth" ? loginSchema : registerSchema,
+    onSubmit: handleSubmit,
+  });
+  return { formik, pathname };
 };
