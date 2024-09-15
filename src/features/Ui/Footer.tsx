867e3059a55b78987dc0bbb239130bad60d3e94f
nachovigilante <nachovigilante@gmail.com>
2024-09-15 17:35:59 -0300

    fixed styles in footer

--- src/features/Ui/Footer.tsx
+++ src/features/Ui/Footer.tsx
@@ -3,21 +3,24 @@ import dvault_navbar from "../../assets/logo_deployvault.png";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col px-8 py-10 text-white bg-accent gap-10">
-      <div className="flex flex-col gap-4">
-        <img src={dvault_navbar} alt="Logo" className="h-6" />
+    <footer className="flex flex-col px-14 pt-20 pb-12 text-white bg-accent gap-16">
+      <div className="flex flex-col gap-10">
+        <img src={dvault_navbar} alt="Logo" width={300}  />
+        <div className="flex flex-col gap-8">
         <div className="flex flex-col items-start">
-          <h5>Address</h5>
+          <h5 className="font-semibold">Address</h5>
           <p>Level 1, 12 Sample St, Sydney NSW 2000</p>
         </div>
         <div className="flex flex-col items-start">
-          <h5>Contact</h5>
+          <h5 className="font-semibold">Contact</h5>
           <ul className="underline">
             <li>1800 123 4567</li>
             <li>info@relume.io</li>
           </ul>
         </div>
-        <div className="flex gap-2">
+
+        </div>
+        <div className="flex gap-6">
           <div className="border h-[30px] w-[30px]" />
           <div className="border h-[30px] w-[30px]" />
           <div className="border h-[30px] w-[30px]" />
@@ -26,8 +29,8 @@ export default function Footer() {
         </div>
       </div>
       <div className="border-t border-t-white flex justify-between pt-3">
-        <span>© 2023 Relume. All rights reserved.</span>
-        <div className="flex gap-2 items-center underline">
+        <span>© 2024 DeployVault. All rights reserved.</span>
+        <div className="flex gap-8 items-center underline text-light">
           <Link to="/">Privacy Policy</Link>
           <Link to="/">Terms of Service</Link>
           <Link to="/">Cookies Settings</Link>
