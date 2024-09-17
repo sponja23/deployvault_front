8a52e0a9c2a05943bfa174e009fffcfe8c07cf73
nachovigilante <nachovigilante@gmail.com>
2024-09-16 21:12:44 -0300

    Generalized Table component

--- src/features/PackageDistribution/PackageDistribution.tsx
+++ src/features/PackageDistribution/PackageDistribution.tsx
@@ -3,6 +3,7 @@ import ShareRepoModal from "./ShareRepoModal";
 import { formatDate } from "../../util/dateHelpers/dateHelpers";
 import { CaosSpinner } from "../../components/CaOSSpinner/CaosSpinner";
 import useUploadedPackages, { UploadedPackage } from "./useUploadedPackages";
+import { Table } from "./Table";
 
 export default function PackageDistribution() {
   const { grantAccess, revokeAccess, packages, isLoading } =
@@ -30,65 +31,68 @@ export default function PackageDistribution() {
     }
   };
 
+  const columns = [
+    {
+      title: "Name",
+      sort: (a: UploadedPackage, b: UploadedPackage) =>
+        a.package_name.localeCompare(b.package_name),
+    },
+    { title: "Description" },
+    {
+      title: "Upload Date",
+      sort: (a: UploadedPackage, b: UploadedPackage) =>
+        new Date(a.created_at).valueOf() - new Date(b.created_at).valueOf(),
+    },
+    {
+      title: "Size",
+      sort: (a: UploadedPackage, b: UploadedPackage) => a.size - b.size,
+    },
+    { title: "Version" },
+    { title: "Accessibility" },
+    { title: "" },
+  ];
+
+  const rowGenerator = (item: UploadedPackage) => (
+    <tr
+      key={item.package_id}
+      className="h-16 even:bg-primary-evens odd:bg-primary-odds"
+    >
+      <td className="px-5 py-3">{item.package_name}</td>
+      <td className="px-5 py-3 max-w-[550px]">
+        <div className="line-clamp-2">{item.description}</div>
+      </td>
+      <td className="px-5 py-3">{formatDate(item.created_at)}</td>
+      <td className="px-5 py-3">{item.size}</td>
+      <td className="px-5 py-3">v{item.version}</td>
+      <td className="px-5 py-3">{item.public ? "Public" : "Private"}</td>
+      <td className="px-5 py-3">
+        <button
+          className="accent-button"
+          onClick={() => {
+            setSelectedPackage(item);
+            setShowShare(true);
+          }}
+        >
+          Manage
+        </button>
+      </td>
+    </tr>
+  );
+
   return isLoading ? (
     <CaosSpinner />
   ) : (
     <div className="w-full h-full flex justify-center px-20 py-20">
-      <div className="w-full flex flex-col gap-5">
-        <div className="flex w-[400px] bg-primary-300 items-center gap-2 px-4 py-[10px] rounded-r-full">
-          <input
-            type="text"
-            placeholder="Search your vault"
-            className="bg-transparent w-full h-full px-0 border-none"
-          />
-          <div className="w-5 h-5">
-            <i className="pi pi-search" />
-          </div>
+      {!packages || packages.length === 0 ? (
+        <div className="text-center">
+          <h1 className="text-2xl font-medium">No packages uploaded yet</h1>
+          <p className="text-lg font-light">
+            Upload a package to start sharing it with your team
+          </p>
         </div>
-        <table className="w-full text-center font-light text-lg">
-          <thead className="bg-primary-300">
-            <tr className="h-14">
-              <th className="font-medium">Name</th>
-              <th className="font-medium">Description</th>
-              <th className="font-medium">Upload Date</th>
-              <th className="font-medium">Size</th>
-              <th className="font-medium">Version</th>
-              <th className="font-medium">Accessibility</th>
-              <th className="font-medium"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {packages?.map((pkg) => (
-              <tr
-                key={pkg.package_id}
-                className="h-16 even:bg-primary-evens odd:bg-primary-odds"
-              >
-                <td className="px-5 py-3">{pkg.package_name}</td>
-                <td className="px-5 py-3 max-w-[550px]">
-                  <div className="line-clamp-2">{pkg.description}</div>
-                </td>
-                <td className="px-5 py-3">{formatDate(pkg.created_at)}</td>
-                <td className="px-5 py-3">{pkg.size}</td>
-                <td className="px-5 py-3">v{pkg.version}</td>
-                <td className="px-5 py-3">
-                  {pkg.public ? "Public" : "Private"}
-                </td>
-                <td className="px-5 py-3">
-                  <button
-                    className="accent-button"
-                    onClick={() => {
-                      setSelectedPackage(pkg);
-                      setShowShare(true);
-                    }}
-                  >
-                    Manage
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      ) : (
+        <Table columns={columns} rows={packages} rowGenerator={rowGenerator} />
+      )}
       <ShareRepoModal
         show={showShare}
         onClose={() => setShowShare(false)}
